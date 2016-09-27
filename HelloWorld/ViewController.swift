85aba4734246e5c822bc41cebd8e369b6a2eaca0
pedrogclp <pedro.gclp@hotmail.com>
2016-09-27 23:19:45 +0200

    Carga Inicial
    Carga inicial curso de Udemy

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -18,8 +18,41 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        let point = CGPoint (x: 5,y: 8)
+        print (point)
+        
+        let rect = CGRect (x: 20, y: 20, width: 100, height: 50)
+        print (rect)
+        
+        let vector = CGVector(dx: 100.0, dy: 0.0)
+        print (vector)
+        
+        let origin = CGPoint.zero
+        print(origin)
+
+        let trans = CGAffineTransform.init(translationX: 20.0, y: 20.0)
+        let rot = CGAffineTransform.init(rotationAngle: CGFloat(M_PI))
+        let scale = CGAffineTransform.init(scaleX: 3.0, y: 0.5)
+        
+        trans.concatenating(rot).concatenating(scale)
+        
+        let hello = "Hello"
+        print(hello)
+        
+        if hello.contains("lo"){
+            print("Hello contiene lo")
+        }
+        
+        let greeting = "Hola me llamo Pedro Jesús García Cabello"
+        
+        for word in greeting.components(separatedBy: " "){
+            print(word)
+        }
+        
+        let hola = "holaaaaaaaaaaaaaaaaaaaaa"
+        
+        print(hola.substring(from : hola.index(hola.startIndex, offsetBy: 10)))
+        
     }
-
-
 }
-
